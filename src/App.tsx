8fdadb8b933a878d437e002e08424575b6889c3a
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-05-27 12:24:42 +0300

    Project added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./Compenents/Header/header";
+import Main from "./Compenents/Main/main";
+
+function App() {
+  return (
+    <div className="text-center h-full">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
